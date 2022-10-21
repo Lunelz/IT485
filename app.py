5b8bed304b1f68b6e048c283c408828fcd2a38c5
Lunelz <nicollerodriguez300@gmail.com>
2022-10-20 21:25:27 -0400

    First commit

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request
+import requests
+import json
+
+# Flask Instance
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def calc_html():
+    if request.method == 'GET':
+      return render_template('calc.html')
+    else:
+      return calc_result()
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
+# Calculator operations
+def calc_result(): 
+  weight_input=request.form['Weight']
+  heightft_input=request.form['HeightFeet']
+  heightin_input=request.form['HeightInch']
+  age_input=request.form['Age']
+  gender=request.form['Gender']
+
+  if weight_input == "" or heightft_input == "" or heightin_input == "" or age_input == "" or gender == "":
+    return render_template(
+      'calc.html',
+      Weight=weight_input,
+      HeightFeet=heightft_input,
+      HeightInch=heightin_input,
+      Age=age_input,
+      Gender=gender,
+      result="Don't leave blank input fields!"
+    )
+
+  weight_input=int(weight_input)
+  heightft_input=int(heightft_input)
+  heightin_input=int(heightin_input)
+  age_input=int(age_input)
+
+  totalinches = (heightft_input * 12) + heightin_input
+
+  if gender == "Woman":
+    BMR = round(655.1 + (4.35 * weight_input) + (4.7 * totalinches) - (4.7 * age_input))
+
+  elif gender == "Man":
+    BMR = round(66.47 + (6.24 * weight_input) + (12.7 * totalinches) - (6.75 * age_input))
+
+  return render_template(
+    'calc.html',
+    Weight=weight_input,
+    HeightFeet=heightft_input,
+    HeightInch=heightin_input,
+    Age=age_input,
+    Gender=gender,
+    result=BMR
+  )
+      
+      
---
+++ data.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+# Create the database and connection
+connection = sqlite3.connect("my_database")
+
+# Create a table for storing data
+connection.execute("CREATE TABLE IF NOT EXISTS My_library (id INTEGER PRIMARY KEY, author STRING, book STRING);")
+
+# Perform CRUD operations
+
+# Create
+connection.execute("INSERT INTO My_library (id,author,book) "
+             "VALUES (1, 'Steve Biko','I write what I like.')")
+
+# Read
+cursor_object = connection.execute("SELECT * FROM My_library")
+print(cursor_object.fetchall())
+
+# Update
+connection.execute("UPDATE My_library SET book = 'I WRITE WHAT I LIKE' WHERE id = 1")
+
+# Delete
+connection.execute("DELETE from My_library WHERE id = 1;")
+
+# Commit changes
+connection.commit()
+
+# Close the connection
+connection.close()
